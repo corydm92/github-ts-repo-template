ad3dae55e70b367b88fe70d6bf3389bb7c715436
Cory Morrissey <corydm92@gmail.com>
2026-02-01 17:45:08 -0800

    feat: ci catching all edge cases, testing bad runtimeTestCopy reference

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,3 +1,5 @@
 import { runtimeTest } from '@pkg/shared-runtime/runtime-test';
+import { runtimeTest as runtimeTestcopy } from '@pkg/shared-runtime/runtime-test copy';
 
 export const backendHello = () => runtimeTest;
+console.log(runtimeTestcopy);
